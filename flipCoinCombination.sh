24cb880f8f0bf89b90e0fa9477c234f448a81b6d
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-02 12:35:40 +0530

    As a simulator, loop through Multiple times of flipping a coin store the Singlet Combination in a Dictionary. Finally determine the percentage of the Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
-coin=$((RANDOM%2))
-if [ $coin -eq 0 ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+headWins=0
+tailWins=0
+count=0
+declare -A singlet
+coinstore=1
+while [[ $count -lt 20 ]]
+do
+	coin=$((RANDOM%2))
+	count=$(($count+1))
+	if [ $coin -eq 0 ]
+	then
+		headWins=$((headWins+1))
+		singlet[$((coinstore++))]="Head"
+	else
+		tailWins=$((tailWins+1))
+		singlet[$((coinstore++))]="Tail"
+	fi
+done
+echo ${!singlet[@]}
+echo ${singlet[@]}
+headPercent=`echo " scale=2; ($headWins/20)*100" | bc`
+tailPercent=`echo " scale=2; ($tailWins/20)*100" | bc`
+echo Heads : $headWins Heads Percentage : $headPercent
+echo Tails : $tailWins Tails Percentage : $tailPercent
 
